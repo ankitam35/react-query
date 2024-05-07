b5b2fbcaf7d7219cb37539d249a8a08999699459
Ankit <ankitmishra.am35@gmail.com>
2024-05-07 09:08:26 +0530

    update

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,13 +2,14 @@ import axios from "axios";
 import { useQuery } from "react-query";
 
 const Home = () => {
-  const { isLoading, data } = useQuery("select-data", () => {
+  const { isLoading, data, isError, error } = useQuery("select-data", () => {
     return axios.get("https://jsonplaceholder.typicode.com/todos/");
   });
 
-console.log(data)
   return isLoading ? (
     <p>Loading</p>
+  ) : isError ? (
+    <p>{error.message}</p>
   ) : (
     data?.data.map((item) => (
       <div>
